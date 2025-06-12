69f027e0ae09042ec921992fc4d8f4b8ebe98d18
Tyler <artinger.tb@gmail.com>
2025-06-11 20:36:45 -0400

    changes

--- .config/waybar/ram.sh
+++ .config/waybar/ram.sh
@@ -1,6 +1,8 @@
-
 while true; do
 	mem_used="$(free -h --si | grep Mem | awk '{print($3)}' | sed 's/.$//')"
+	if [[ $mem_used == *"." ]]; then
+		mem_used="$(awk -v var="$mem_used" 'BEGIN {printf "%.1f", var/1000}')" 
+	fi
 	mem_total="$(free -h --si | grep Mem | awk '{print($2)}' | sed 's/.$//')"
 	echo "${mem_used}/${mem_total}"
 	sleep 1
